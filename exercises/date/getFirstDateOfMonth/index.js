c23141dcff05b5995a105d8c1c19150e23192df3
AlekseyDanchin <aleksey.danchin@gmail.com>
2020-08-31 17:44:22 +0300

    release

--- exercises/date/getFirstDateOfMonth/index.js
+++ exercises/date/getFirstDateOfMonth/index.js
@@ -1,3 +1,1 @@
-module.exports = function getFirstDayOfMonth(year, month) {
-	return new Date(year, month, 1);
-};
+module.exports = 
--- exercises/date/getLastDateOfMonth/index.js
+++ exercises/date/getLastDateOfMonth/index.js
@@ -1,3 +1,1 @@
-module.exports = function getLastDateOfMonth(year, month) {
-	return new Date(year, month + 1, 0);
-};
+module.exports = 
